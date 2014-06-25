ee9f20e917814c298d1e6068ebf7d119a47b7ecd
Jeremy <zombieslap360@gmail.com>
2014-06-25 17:46:15 -0300

    Single Door Animation
    Started the scripts for animation.

---
+++ Assets/Scripts/Inventory/PlayerKey.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerKey : MonoBehaviour {
+	public bool playerHasKey = false;
+}
---
+++ Assets/Temp.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class Temp : MonoBehaviour {
+	public float moveSpeed;
+	public float rotateSpeed;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("w")) {
+			transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+		}
+		
+		if(Input.GetKey("s")) {
+			transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+		}
+		
+		if(Input.GetKey("a")) {
+			transform.Rotate(Vector3.down * rotateSpeed * Time.deltaTime);
+		}
+		
+		if(Input.GetKey("d")) {
+			transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+		}
+	}
+}
